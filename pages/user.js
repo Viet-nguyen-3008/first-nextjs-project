c2a1c5ad3de13649816b2dfb91954bc623fbbb2e
Vietnguyen <Cshs30081993@gmail.com>
2020-11-28 15:11:43 +0700

    commit in new branch

--- pages/user.js
+++ pages/user.js
@@ -4,7 +4,7 @@ export default class User extends Component {
     render() {
         return (
             <div>
-                <h1>Hê lố</h1>
+                <h1>New Branch</h1>
             </div>
         )
     }
